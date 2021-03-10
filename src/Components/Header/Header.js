c5fef391cd8457218b6539e3de2a868352dec72a
Your Name <you@example.com>
2021-03-10 09:31:47 +0600

    again try

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -15,20 +15,20 @@ const Header = () => {
         <>
             <div className="container">
                 <nav className="navbar navbar-expand-lg">
-                    <a className="navbar-brand logo" href="#"><img src={logo2} alt="" /></a>
+                    <a className="navbar-brand logo" href="#navbarNav"><img src={logo2} alt="" /></a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse menu" id="navbarNav">
                         <ul className="navbar-nav justify-content-end">
                             <li className="nav-item">
-                                <a className="nav-link" href="#"><span className="icon">0</span> <FontAwesomeIcon className="icon2" icon={faShoppingCart} /></a>
+                                <a className="nav-link" href="#navbarNav"><span className="icon">0</span> <FontAwesomeIcon className="icon2" icon={faShoppingCart} /></a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Log in</a>
+                                <a className="nav-link" href="#navbarNav">Log in</a>
                             </li>
                             <li className="nav-item active">
-                                <a className="nav-link" href="#">Sign Up<span className="sr-only">(current)</span></a>
+                                <a className="nav-link" href="#navbarNav">Sign Up<span className="sr-only">(current)</span></a>
                             </li>
                         </ul>
                     </div>
@@ -51,7 +51,7 @@ const Header = () => {
             <div className="container px-2">
                 <div className="row">
                     <div className="col-md-12 row">
-                        <FoodItems />
+                        <FoodItems category={category} />
                     </div>
                 </div>
                 <button className="btn btn-sm btn-primary text-center">Proceed Check Out</button>
